1306b8a49d7771b9c3e0c82370945fb8415936f3
Cristian Iranzo <iranzocristian@gmail.com>
2025-07-02 13:10:39 +0200

    Changes to complete the big_data ex

--- R/benchmark.R
+++ R/benchmark.R
@@ -3,7 +3,6 @@ library(dplyr)
 library(bench)
 library(parallel)
 library(mapSpain)
-library(ggplot2)
 
 # Note: This is not an R project so we need to configure the root path
 root <- "C:/copia_seguridad/cursos/CDG/practicas/04_github/big_data_training/"
@@ -16,12 +15,6 @@ zgz <- esp_get_prov("Zaragoza")
 # Convert the CRS to the daily gpkg crs
 zgz_25830 <- st_transform(zgz, 25830)
 
-# Plot it to check
-ggplot(zgz) +
-  geom_sf(fill = "darkgreen", color = "white", alpha=0.5) +
-  geom_sf(data = test) +
-  theme_bw()
-
 # Create the function to do the averaging by file in parallel
 compute_avg <- function(file_path) {
   
@@ -30,34 +23,27 @@ compute_avg <- function(file_path) {
   
   # Construct query
   cols <- paste(c("MeanTemperature", "Precipitation", "MeanRelativeHumidity",
-            "day", "geom"), collapse = ",")
+                  "day", "geom"), collapse = ",")
   sql_q <- paste0(c('SELECT ', cols, ' FROM \"', lname, '\"'), collapse="")
   
   # Load only Zaragoza points
   i_df <- st_read(file_path, quiet = TRUE,
-                    query = sql_q,
-                    wkt_filter = st_as_text(zgz_25830$geometry)
-            )
+                  query = sql_q,
+                  wkt_filter = st_as_text(zgz_25830$geometry)
+  )
   # Compute the average
   i_avg <- i_df |>
     st_drop_geometry() |>
     group_by(day) |>
     summarise(across(where(is.numeric), mean, na.rm = TRUE))
-
+  
   # Compute and return averages
   return(i_avg)
 }
 
-# Average MeanTemperature, Precipitation and RelativeHumidity values
-# for Zaragoza
-system.time({
-  daily_avg <- mclapply(zgz_daily_files, compute_avg, mc.cores = 2)
-})
-
-# The above object is the average of all the stations each day
-# Now perform the average of averages to obtain the province average
-daily_avg_df <- do.call(rbind, daily_avg)
-zgz_avg <- colMeans(daily_avg_df[2:4])
-
-# Export to csv
-write.csv(t(data.frame(zgz_avg)), file.path(root, "data/results.csv"), row.names=FALSE)
+# Benchmark how many cores are optimal
+averaging <- bench::mark(
+  "ncores_1" = mclapply(zgz_daily_files, compute_avg, mc.cores = 1),
+  "ncores_2" = mclapply(zgz_daily_files, compute_avg, mc.cores = 2),
+  "ncores_3" = mclapply(zgz_daily_files, compute_avg, mc.cores = 3)
+)
